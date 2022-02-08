80777fce0e8507a9c6a6a41c393b9babf1e62fa8
Jaeden West <jaeden.t.west@gmail.com>
2022-02-07 17:42:03 -0700

    home made assertion function

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🤬(╯°□°）╯︵ ┻━┻🤬 Assertion Failed: " + actual + " !== " + expected);
+  } else if (actual === expected) {
+    console.log("🤑(⌐■_■)🤑 Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 3);
