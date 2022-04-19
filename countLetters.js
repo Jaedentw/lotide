9a6530f293f1dffa72318419279adccf3efe15c1
Jaeden West <jaeden.t.west@gmail.com>
2022-04-19 13:40:14 -0600

    clean up

--- countLetters.js
+++ countLetters.js
@@ -1,29 +1,26 @@
-const countOnly = require('./countOnly');
+//const countOnly = require('./countOnly');
 
 const countLetters = function(string) {
   
-  let count = 0;
   const scores = {};
-  let keys = Object.keys(scores);
 
-  for (let l = 0; l < string.length; l++) {
-    for (let x = 0; x <= keys.length; x++) {
-      if (string[l] !== keys[x] && string[l] !== ' ') {
-        count += 1;
-        if (count >= keys.length) {
-          scores[string[l]] = true;
-          count = count - count;
-        }
+  for (let letter of string) {
+    if (letter !== ' ') {
+      if (scores[letter]) {
+        scores[letter] += 1;
+      } else {
+        scores[letter] = 1;
       }
     }
   }
-  const final = countOnly(string, scores);
-  return final;
+
+  return scores;
+  
 };
 
 module.exports = countLetters;
 
-//console.log(countLetters('mississippy mississippy'));
+console.log(countLetters('mississippy mississippy'));
 
 /*
 const countLetters = function(string) {
