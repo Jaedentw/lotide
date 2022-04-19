7cab5281233189fd47a3ac79d6a50b5ae02f6611
Jaeden West <jaeden.t.west@gmail.com>
2022-04-19 13:44:47 -0600

    more cleanup

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -2,16 +2,10 @@
 
 const findKeyByValue = function(object1, value1) {
   const o1Keys = Object.keys(object1);
-  let count = 0;
+  
   for (let key of o1Keys) {
     if (object1[key] === value1) {
       return key;
-    }
-    if (object1[key] !== value1) {
-      count += 1;
-      if (count === o1Keys.length) {
-        return undefined;
-      }
     }
   }
 };
