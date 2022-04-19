a83a84629defe7388b75d1b71058e7695fd6bd1e
Jaeden West <jaeden.t.west@gmail.com>
2022-04-19 13:24:54 -0600

    alterations

--- countLetters.js
+++ countLetters.js
@@ -24,3 +24,26 @@ const countLetters = function(string) {
 module.exports = countLetters;
 
 //console.log(countLetters('mississippy mississippy'));
+
+/*
+const countLetters = function(string) {
+  
+  let count = 0;
+  const scores = {};
+  let keys = Object.keys(scores);
+
+  for (let l = 0; l < string.length; l++) {
+    for (let x = 0; x <= keys.length; x++) {
+      if (string[l] !== keys[x] && string[l] !== ' ') {
+        count += 1;
+        if (count >= keys.length) {
+          scores[string[l]] = true;
+          count = count - count;
+        }
+      }
+    }
+  }
+  const final = countOnly(string, scores);
+  return final;
+};
+*/
--- eqArrays.js
+++ eqArrays.js
@@ -5,17 +5,12 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
-  let count = 0;
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
-    } else {
-      count += 1;
-    }
+    } 
   }
-  if (count === array1.length) {
-    return true;
-  }
+  return true;
 };
 
 module.exports = eqArrays;
