95b9a4a9995adcdc7b326df818e87340ee0c75f3
Jaeden West <jaeden.t.west@gmail.com>
2022-04-19 13:52:06 -0600

    without.js simplified

--- without.js
+++ without.js
@@ -2,6 +2,28 @@
 
 const without = function(source, itemsToRemove) {
   let permitted = [];
+  
+  for (let ele1 of source) {
+    for(let ele2 of itemsToRemove) {
+      if (ele1 !== ele2) {
+        permitted.push(ele1);
+      }
+    }
+  }
+
+  return permitted;
+};
+
+module.exports = without;
+
+/*
+const nums = [1, 2, 3];
+console.log(without(nums, [2]));
+assertArraysEqual(nums, [1, 2, 3])
+
+
+const without = function(source, itemsToRemove) {
+  let permitted = [];
   for (let i = 0; i < source.length; i++) {
     let count = 0;
     for (let y = 0; y < itemsToRemove.length; y++) {
@@ -15,11 +37,4 @@ const without = function(source, itemsToRemove) {
   }
   return permitted;
 };
-
-module.exports = without;
-
-/*
-const nums = [1, 2, 3];
-console.log(without(nums, [2]));
-assertArraysEqual(nums, [1, 2, 3])
 */
